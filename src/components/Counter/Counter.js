036ee061ed303961c05ebe4ddebe52b35ae4cc47
Serhi Hats <gatsserv@gmail.com>
2023-04-29 02:40:49 +0300

    add color number on count

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,57 +2,43 @@ import {Component} from "react";
 import "./Counter.css";
 
 class Counter extends Component {
-timeId = 0;
+  timeUpdate;
 
   state = {
     count: 0,
     color: "black",
     arrayOfColor: ["red", "black", "green"],
     index: 1,
-    // ddd: 1,
+    visible: true,
   }
 
   handlerCounter(operand = 1) {
-    let index = (operand === -1) ? 0 : (operand ===
-      this.state.count * -1) ? 1 : 2;
+    let index = (operand === -1) ? 0
+      : (operand === this.state.count * -1) ? 1 : 2;
+
     this.setState({
       index: index,
       count: this.state.count + operand,
-      // ddd: new Date(),
-    })
-
-    console.log(new Date().toLocaleTimeString() )
-    console.log(new Date().toLocaleDateString());
-    let d = new Date().toLocaleDateString();
-    let t = new Date().toLocaleTimeString();
-
-    let ad = d.split("\.");
-    console.log('ad:',ad);
-    let at = t.split(":");
-    console.log('at:',at);
-    ad.concat(at);
-
-    console.log(ad);
-
-
-    console.log(`${ad[0]}:${ad[1]}:${ad[2]}:${at[0]}:${at[1]}:${at[2]}`);
-    console.log(new Date().toLocaleDateString() + `:${at[0]}:${at[1]}:${at[2]}`);
-    // this.timeId = setTimeout(this.setDefaultColor, 4000);
+    });
   }
 
-  index = 1;
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.timeUpdate = new Date();
+    setTimeout(() => {
+      this.setDefaultColor()
+    }, 2000);
+  }
 
   setDefaultColor() {
-    if ((new Date()- this.state.ddd) > 3500) {
-      this.setState({
-        index: 1
-      });
+    if ((new Date() - this.timeUpdate) > 1500) {
+      this.setState({index: 1});
     }
   }
 
   render() {
     return (
       <div className="Counter">
+        {/*<button>Visible</button>*/}
         <button onClick={() => this.handlerCounter()}>Incriment</button>
         <button onClick={() => this.handlerCounter(-1)}>Decrement</button>
         <button onClick={() => this.handlerCounter(this.state.count * -1)}>Reset</button>
@@ -60,7 +46,6 @@ timeId = 0;
       </div>
     );
   }
-
 }
 
 export default Counter;
