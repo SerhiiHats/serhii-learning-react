25ffd22a057cf30d3864c4379d32c12ad0c8f961
Serhi Hats <gatsserv@gmail.com>
2023-04-29 17:00:12 +0300

    add div with img like on conditional rendering

--- src/components/ConditionalRendering/ConditionalRendering.js
+++ src/components/ConditionalRendering/ConditionalRendering.js
@@ -1,15 +1,29 @@
 import {Component} from "react";
 import "./ConditionalRendering.css";
+import Counter from "../Counter/Counter";
+import like_1 from "../../images/like_1.svg"
+import like_2 from "../../images/like_2.svg"
 
 class ConditionalRendering extends Component{
- messages = ["hello", "Hi, how are you", "Coll me"];
+   messages = ["hello", "Hi, how are you", "Coll me"];
   // messages = []
+  state ={
+    counterVisible : true,
+    visibleImg:false,
+  }
 
   render() {
     return(
-      <div>
+      <div className={"ConditionalRendering"}>
         <span>Hello </span>
         {this.messages.length >0 && <span>, you have {this.messages.length} unread messages</span>}
+        <div className={"counter-visible"}>
+          <button onClick={()=>this.setState({counterVisible: !this.state.counterVisible})}>Visible Of Counter</button>
+        </div>
+        {this.state.counterVisible && <Counter/>}
+        <div className={"like"} onClick={()=>{this.setState((state)=>({visibleImg: !state.visibleImg}))}}>
+          <img src={this.state.visibleImg? like_2 : like_1} alt={"like"}/>
+        </div>
       </div>
     )
   }
