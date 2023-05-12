db3fbe1bec3f9563e5de8efffcee6e2ad5296d0e
Serhi Hats <gatsserv@gmail.com>
2023-05-13 00:09:28 +0300

    initial commit counter with redux

---
+++ src/components/CounterRedux/CounterRedux.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./CounterRedux.cssgit "
+
+class CounterRedux extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default CounterRedux;
