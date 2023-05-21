ea81ef8620c6333eb74684b4027a0eaaec6bdc68
Serhi Hats <gatsserv@gmail.com>
2023-05-22 01:53:23 +0300

    add final style for todo list composition

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from "../Button/Button";
 import {iconTypes} from "../../constants/icons";
 import styles from "./ListItem.module.scss"
@@ -7,13 +7,23 @@ const ListItem = (props) => {
 
   const {key, id, elem, removeItem} = props;
 
+  const [done, setDone] = useState(false);
+
   function onClickRemove() {
     removeItem(id);
   }
 
+  function handlerCheckBox() {
+    setDone(!done);
+    console.log(done);
+  }
+
   return (
     <li className={styles.containerItem} key={key} id={id}>
-      {elem}
+      <div className={done ? styles.done : styles.unDone}>
+        <input type={"checkbox"} onChange={handlerCheckBox} checked={done}/>
+        {elem}
+      </div>
       <Button size={"small"} onClick={() => onClickRemove()} iconType={iconTypes.cross}/>
     </li>
   )
