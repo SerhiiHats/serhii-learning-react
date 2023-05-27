8baa783e51592b4277feb821ea1d1cb8520d7c90
Serhi Hats <gatsserv@gmail.com>
2023-05-28 01:35:31 +0300

    add style fir link

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -6,24 +6,23 @@ import BlogPage from "../../pages/BlogPage";
 import AboutPage from "../../pages/AboutPage";
 import NotfoundPage from "../../pages/NotfoundPage";
 import {Link} from "react-router-dom";
+import Layout from "../../router/Layout";
 
 const AppRouter = () => {
   return (
     <>
-     <header className={style.header}>
-       <Link to={"/"}>Home</Link>
-       <Link to={"/posts"}>Blog</Link>
-       <Link to={"/about"}>About</Link>
-       {/*<a href={"/about"}>About</a>*/}
-     </header>
+
       {/*<div className={style.div}>*/}
       {/*  <h1>Get started with React-Router 6</h1>*/}
       {/*</div>*/}
       <Routes>
-        <Route path={"/"} element={<HomePage/>}/>
-        <Route path={"/posts"} element={<BlogPage/>}/>
-        <Route path={"/about"} element={<AboutPage/>}/>
-        <Route path={"*"} element={<NotfoundPage/>}/>
+        <Route path={"/"} element={<Layout/>}>
+          {/*<Route path={"/"} element={<HomePage/>}/>*/}
+          <Route index element={<HomePage/>}/>
+          <Route path={"/posts"} element={<BlogPage/>}/>
+          <Route path={"/about"} element={<AboutPage/>}/>
+          <Route path={"*"} element={<NotfoundPage/>}/>
+        </Route>
       </Routes>
     </>
   );
--- src/router/Layout.js
+++ src/router/Layout.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import style from "../components/AppRouter/AppRouter.module.scss";
+import {Link, NavLink, Outlet} from "react-router-dom";
 
 const Layout = () => {
   return (
-    <section className={"container"}>
+    <>
+      <header className={style.header}>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/posts"}>Blog</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        {/*<Link to={"/about"}>About</Link>*/}
+        {/*<a href={"/about"}>About</a>*/}
+      </header>
 
-    </section>
+      <main className={style.containerMain}>
+        <Outlet/>
+      </main>
+
+      <footer className={style.footer}>2023 - my footer</footer>
+    </>
   );
 };
 
